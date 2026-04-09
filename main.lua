73cfe0d58c924766b0bb63117f66c69bca6ab0ff
rezishon <rezashahmoradi.81@outlook.com>
2026-04-09 22:07:37 +0330

    feat(main.lua): start of project
    For now:
    - Gets the series directory and files formats as args.
    - Handles some possible errors for now.
    - Find corroct files
    - some todos & test codes & so on

---
+++ main.lua
@@ -0,0 +1,42 @@
+local dir_path = arg[1]
+local dir_list = ""
+local files_format = arg[2]
+
+if dir_path == nil then
+	io.stderr:write("\n\27[31mPlease give the series directory path as an argument\nUse -h or --help for mor info\n\n")
+	os.exit(1, true)
+end
+
+if dir_path:match("--help=") or dir_path:match("-h") then
+	io.stdout:write("\nargs:\n1: series directory path\n2(optional): files name format\n\n")
+	os.exit(1, true)
+end
+
+if io.open(dir_path, "r") == nil then
+	io.stderr:write("\n\27[31mInvalid directory path given: " .. dir_path .. "\n\n")
+	os.exit(1, true)
+end
+
+local tmp_dir_list = io.popen("ls " .. dir_path)
+dir_list = tmp_dir_list:read("*a")
+tmp_dir_list:close()
+
+-- TODO: check if files format inserted =>
+-- place it in the following gmatch
+-- TODO: save data in a hidden file
+-- TODO: if metadata file presents =>
+-- take the data from there, needed to
+-- handle things on format inserted or not
+-- and overwrite or not
+-- TODO: Check how to know what length of video
+-- does watched automatically
+local tb = {}
+for match in string.gmatch(dir_list, "%s*E03%s*") do
+	table.insert(tb, match)
+end
+
+for _, v in ipairs(tb) do
+	print(v)
+end
+-- end
+-- print(string.match(dir_list, "^Mm%s*"))
