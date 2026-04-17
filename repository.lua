9ace85e829bad954aa6ae0028a857fb6e29f424c
rezishon <rezashahmoradi.81@outlook.com>
2026-04-17 13:32:09 +0330

    feat(repository.lua): add `path_fixer` function
    This will handle dublicate way of directory passing to our args.
    Two ways of passing a directory path:
    - ../directory/
    - ../directory

--- repository.lua
+++ repository.lua
@@ -4,4 +4,8 @@ Repository.database_path = function(dir_path)
 	return dir_path .. "/.database.json"
 end
 
+Repository.path_fixer = function(path)
+	return path:gsub("%s*//%s*", "/")
+end
+
 return Repository
