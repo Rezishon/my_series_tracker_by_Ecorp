41f47cd06cbaa15fcc477ece74ff420df77d1193
rezishon <rezashahmoradi.81@outlook.com>
2026-04-17 13:37:36 +0330

    feat(main.lua + repository.lua): add `dir_path_validator` function
    This will validate multiple possible errors from passing a path to our
    arg

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 local repo = require("repository")
 
 local dir_path = arg[1]
+repo.dir_path_validator(dir_path)
 local database_path = repo.path_fixer(repo.database_path(dir_path))
 local metadata_path = repo.path_fixer(repo.metadata_path(dir_path))
 local dir_list = ""
--- repository.lua
+++ repository.lua
@@ -8,6 +8,23 @@ Repository.metadata_path = function(dir_path)
 	return dir_path .. "/.metadata.json"
 end
 
+Repository.dir_path_validator = function(dir_path)
+	if dir_path == nil then
+		io.stderr:write(
+			"\n\27[31mPlease give the series directory path as an argument\nUse -h or --help for mor info\n\n"
+		)
+		os.exit(1, true)
+	elseif dir_path:match("--help=") or dir_path:match("-h") then
+		io.stdout:write("\nargs:\n1: series directory path\n2: Season pattern\n3: Episode pattern\n\n")
+		os.exit(1, true)
+	end
+
+	if io.open(dir_path, "r") == nil then
+		io.stderr:write("\n\27[31mInvalid directory path given: " .. dir_path .. "\n\n")
+		os.exit(1, true)
+	end
+end
+
 Repository.path_fixer = function(path)
 	return path:gsub("%s*//%s*", "/")
 end
