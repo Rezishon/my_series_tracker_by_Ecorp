44da9d51277295ac4dabd91da0ccfb2f8bdb1889
rezishon <rezashahmoradi.81@outlook.com>
2026-06-05 21:23:12 +0330

    feat(watch_tracker.lua): add error handling
    By reading the database file, we will find any "not found" items that my
    comes from user.

--- watch_tracker.lua
+++ watch_tracker.lua
@@ -29,3 +29,24 @@ for match in string.gmatch(file_name, metadata.season_pattern .. metadata.episod
 	tmp_file_metadata.episode = string.match(match, metadata.episode_pattern)
 end
 
+local database_file = io.open(database_path, "r")
+local database_line_by_line = repo.read_file_line_by_line(database_file)
+local season_validation_flag = true
+local episode_validation_flag = true
+
+for _, v in ipairs(database_line_by_line) do
+	if string.match(v, metadata_key_patterns.season_key_pattern) then
+		print("found season")
+		season_validation_flag = false
+	elseif string.match(v, metadata_key_patterns.episode_key_pattern) then
+		print("found episode")
+		episode_validation_flag = false
+	end
+end
+
+print(season_validation_flag, episode_validation_flag)
+if season_validation_flag == false or episode_validation_flag == false then
+	io.stderr:write("\n\27[31mThe given file didn't found!\n")
+	os.exit(1, true)
+end
+
