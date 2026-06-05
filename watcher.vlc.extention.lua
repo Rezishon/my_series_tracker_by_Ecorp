33bf05ec76f94edd3554a38a746c362853716f2c
rezishon <rezashahmoradi.81@outlook.com>
2026-06-05 21:41:04 +0330

    feat(watcher.vlc.extention.lua): add `file_name_finder` func
    This function will fix the file user to make sure that it won't break on
    wrong user arg.
    The path to home directory should be configed.

--- watcher.vlc.extention.lua
+++ watcher.vlc.extention.lua
@@ -1,3 +1,12 @@
+local function file_name_finder()
+	local item = vlc.input.item()
+	if not item then
+		return nil
+	end
+	local tmp_value = string.gsub(item:uri(), "file:///%w*/user", "~")
+	return string.match(tmp_value, "([^/]+)$")
+end
+
 local function path_finder(item_url)
 	local fixed_item_url = ""
 	fixed_item_url = string.gsub(item_url, "file:///%w*/user", "~")
